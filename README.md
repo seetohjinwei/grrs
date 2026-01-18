45f874158d7a3868f9324ee2c13f966b4e144d72
Jin Wei <seetohjinwei@gmail.com>
2026-01-18 15:06:04 +0800

    support various options in find_matches

--- README.md
+++ README.md
@@ -2,6 +2,15 @@
 
 Follows the tutorial from: https://rust-cli.github.io/book/index.html 
 
+## Features
+
+`grep` defaults are not very ergonomic by modern standards, so grrs steals some ideas from other tools like ripgrep.
+
+* Supports recursive by default
+    * Passing a directory will naturally search all files in that directory
+    * Use `--depth` to control the max depth
+* Shows line numbers by default
+
 ```sh
 RUST_LOG=debug cargo run -- pattern src/main.rs
 ```
--- src/main.rs
+++ src/main.rs
@@ -5,20 +5,39 @@ use log::debug;
 #[derive(Parser)]
 struct Args {
     pattern: String,
-    path: std::path::PathBuf,
+    path: Option<std::path::PathBuf>,
+
+    // Flags
+    #[arg(short = 'd', long = "max-depth", default_value_t = -1, help = "Limits the depth of directory traversal. -1 (default) to disable the maximum. 0 to disable recursion.")]
+    max_depth: i32,
+    #[arg(short = 'N', long = "no-line-number", default_value_t = false)]
+    no_line_numbers: bool,
+    #[arg(short = 'i', long = "ignore-case", default_value_t = false, help = "ignore case")]
+    ignore_case: bool,
 }
 
 fn main() -> Result<()> {
     env_logger::init();
 
     let args = Args::parse();
+    let path = args.path.unwrap_or(std::env::current_dir()?);
 
-    let f = std::fs::File::open(&args.path)
-        .with_context(|| format!("could not read file {:?}", &args.path))?;
+    let f = std::fs::File::open(&path)
+        .with_context(|| format!("could not read file {:?}", path))?;
     let reader = std::io::BufReader::new(f);
 
-    debug!("Searching for {}", &args.pattern);
-    grrs::find_matches(reader, std::io::stdout(), &args.pattern)?;
+    // TODO: Handle directories and max-depth
+
+    debug!("Searching for {} in {:?}", &args.pattern, path);
+    grrs::find_matches(
+        reader,
+        std::io::stdout(),
+        &args.pattern,
+        &grrs::MatchOptions {
+            show_line_numbers: !args.no_line_numbers,
+            case_insensitive: args.ignore_case,
+        },
+    )?;
 
     Ok(())
 }
